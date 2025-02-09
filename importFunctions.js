b5b0238d194157ff1969b820e04f0ba07ba6dbcb
AshishTrueIg <aagrawal@trueigtech.com>
2025-02-09 17:11:36 +0530

    Node.js Training Started : Completed my first program and import module with hands on exercise

---
+++ importFunctions.js
@@ -0,0 +1,7 @@
+const functions = require('./functions')
+
+console.log(functions.add(23,37));
+console.log(functions.sub(23,37));
+console.log(functions.multiply(23,37));
+console.log(functions.divide(23,37));
+
